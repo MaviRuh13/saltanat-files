b5d7e25958a1b2e32c72845c9c6cad39333ff9e7
MaviRuh13 <61208397+MaviRuh13@users.noreply.github.com>
2022-06-12 17:46:35 +0300

    12.06.2022 systems
    12.06.2022 systems

---
+++ systems/Mavi efsun/README.md
@@ -0,0 +1,1 @@
+![image](https://user-images.githubusercontent.com/61208397/173238654-a32ba44e-1f7e-40d9-bd2e-65e1597ca185.png)
---
+++ systems/Mavi efsun/root/uitooltip.py
@@ -0,0 +1,9 @@
+#Arat 
+
+	SPECIAL_POSITIVE_COLOR = grp.GenerateColor(0.6911, 0.8754, 0.7068, 1.0)
+	SPECIAL_POSITIVE_COLOR2 = grp.GenerateColor(0.8824, 0.9804, 0.8824, 1.0)
+
+#Değiştir.
+
+	SPECIAL_POSITIVE_COLOR = grp.GenerateColor(0,1.75,1, 1.0)
+	SPECIAL_POSITIVE_COLOR2 = grp.GenerateColor(0.8824, 0.9804, 0.8824, 1.0)
